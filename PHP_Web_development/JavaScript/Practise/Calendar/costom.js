3cbf60c41ad4bb4744b79d9c2313c242a0e83a1e
hareom284 <zawzawwin@ucsloikaw.edu.mm>
2020-10-03 00:57:41 +0630

    added

---
+++ PHP_Web_development/JavaScript/Practise/Calendar/costom.js
@@ -0,0 +1,209 @@
+// loading in javascript 
+window.onload = function one() {
+
+    var bar = document.getElementById("bar"); //got the bar
+    var width = 0; //got the width
+    var lag = setInterval(load, 100); //set the var lag to providee intetval
+    var main = document.getElementById("main"); //to be shown during loading
+    var content = document.getElementById("content"); //to be shown after loading
+    var h3 = document.getElementById("para");
+    var h4 = document.getElementById("hatodo");
+
+    function load() {
+        if (width >= 100) { //after 100% loading events
+            clearInterval(lag);
+            main.style.display = 'none';
+            content.style.display = 'block';
+
+        } else { //during loading events
+            width++;
+            bar.style.width = width + '%';
+            h3.innerHTML = width + '% loaded';
+
+            // i have made quite a use of switch here
+            switch (width) {
+
+                case 1:
+                    h4.innerHTML = "initiating....";
+                    break;
+
+                case 10:
+                    h4.innerHTML = "getting titles....";
+                    break;
+
+                case 30:
+                    h4.innerHTML = "titles ready.<br/>recieving files....";
+                    break;
+
+                case 50:
+                    h4.innerHTML = "titles ready<br/>files ready.<br/>recieving data...."
+                    break;
+
+                case 70:
+                    h4.innerHTML = "titles ready<br/>files ready.<br/>data ready.</br>generating document"
+                    break;
+
+                case 75:
+                    h4.innerHTML = "titles ready<br/>files ready.<br/>data ready.</br>generating document...."
+                    break;
+
+                case 80:
+                    h4.innerHTML = "titles ready<br/>files ready.<br/>data ready.</br>generating document..."
+                    break;
+
+                case 85:
+                    h4.innerHTML = "titles ready<br/>files ready.<br/>data ready.</br>generating document.."
+                    break;
+
+                case 90:
+                    h4.innerHTML = "titles ready<br/>files ready.<br/>data ready.</br>generating document."
+                    break;
+
+                case 95:
+                    h4.innerHTML = "titles ready<br/>files ready.<br/>data ready.</br>generating document.."
+                    break;
+
+                case 98:
+                    h4.innerHTML = "titles ready<br/>files ready.<br/>data ready.</br>generating document...."
+                    break;
+                    //you can mess arond with this removing the break statements
+
+            }
+        }
+    }
+    getDat();
+
+}
+
+function getDat() {
+    let date = new Date();
+    let weekday = date.getDay();
+    let day = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    // console.log(month);
+    console.log(day);
+    console.log(weekday);
+    let weekDay = ["sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    // console.log(weekDay[day]);
+    let Month = ["January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    // console.log(Month[month]);
+    //setting day month and year to header
+    //get date from window date and time 
+    document.getElementById("month").innerHTML = Month[month];
+    document.getElementById("year").innerHTML = year;
+    document.getElementById("day").innerHTML = day;
+    // get number of day in one month 
+
+    //adding nOfDays in 
+    function nOfDays(month, year) {
+        return new Date(year, month, 0).getDate();
+    }
+    var nOfDay = nOfDays(month, year);
+    // console.log(nOfDay);
+    var shit = 0;
+    var generate = "";
+    // console.log(day);
+    if (weekday == 0) {
+        if (day == 1) {
+            generate += `<ul>
+           <li>${++shit}</li>
+           <li> ${++shit}</li>
+           <li> ${++shit}</li>
+           <li> ${++shit}</li>
+           <li> ${++shit}</li>
+           <li>${++shit}</li>
+           <li>${++shit}</li>
+           </ul>`;
+        } else if (day > 1) {
+
+        }
+    } else if (weekday == 1) {
+        generate += `<ul>
+           <li></li>
+           <li> ${++shit}</li>
+           <li> ${++shit}</li>
+           <li> ${++shit}</li>
+           <li> ${++shit}</li>
+           <li>${++shit}</li>
+           <li>${++shit}</li>
+           </ul>`;
+    } else if (weekday == 2) {
+        generate += `<ul>
+           <li></li>
+           <li> </li>
+           <li>${++shit} </li>
+           <li> ${++shit}</li>
+           <li> ${++shit}</li>
+           <li>${++shit}</li>
+           <li>${++shit}</li>
+           </ul>`;
+    } else if (weekday == 3) {
+        generate += `<ul>
+           <li></li>
+           <li> </li>
+           <li> </li>
+           <li> ${++shit}</li>
+           <li> ${++shit}</li>
+           <li>${++shit}</li>
+           <li>${++shit}</li>
+           </ul>`;
+    } else if (weekday == 4) {
+        generate += `<ul>
+           <li></li>
+           <li> </li>
+           <li> </li>
+           <li> </li>
+           <li> ${++shit}</li>
+           <li>${++shit}</li>
+           <li>${++shit}</li>
+           </ul>`;
+    } else if (weekday == 5) {
+        generate += `<ul>
+           <li></li>
+           <li> </li>
+           <li> </li>
+           <li> </li>
+           <li> </li>
+           <li>${++shit}</li>
+           <li>${++shit}</li>
+           </ul>`;
+    } else {
+        generate += `<ul>
+           <li></li>
+           <li> </li>
+           <li> </li>
+           <li> </li>
+           <li> </li>
+           <li>${++shit}</li>
+           <li>${++shit}</li>
+           </ul>`;
+    }
+    console.log(generate);
+    document.getElementById("generate").innerHTML = generate;
+
+}
+for (let i = 0; i < 7; i++) {
+    for (let j = 0; j < 7; j++) {
+        console.log('okay');
+    }
+}
